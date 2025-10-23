8b73612fa277c2a2cba3883538dc23a99e3942e2
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-10-22 20:36:15 -0400

    chore: update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
+import gulp from 'gulp';
 import browserSyncLib from 'browser-sync';
 import { deleteSync } from 'del';
 import htmlMin from 'gulp-html-minifier-terser';
 import sharpOptimizeImages from 'gulp-sharp-optimize-images';
 import webp from 'gulp-webp';
-import gulp from 'gulp';
 import sass from 'sass';
 import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
-import sourcemaps from 'gulp-sourcemaps';
 import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
 
@@ -17,86 +16,85 @@ const { src, dest, series, parallel, watch } = gulp;
 const browserSync = browserSyncLib.create();
 const compiledSass = gulpSass(sass);
 
-// ---------- BrowserSync ----------
+/* ---------- BrowserSync ---------- */
 export const server = () => {
   browserSync.init({
     server: { baseDir: 'dist/' },
+    notify: false,
   });
   watch('dist/*.html').on('change', browserSync.reload);
 };
 
-// ---------- Clean ----------------
-export const clean = (done) => {
-  deleteSync('dist');
-  done();
-};
+/* ---------- Clean ---------- */
+export const clean = () => deleteSync(['dist']);
 
-// ---------- Styles ----------
+/* ---------- Styles ---------- */
 export const styles = () => {
-  return src('src/sass/style.scss')
+  return src('src/sass/style.scss', { sourcemaps: true })
     .pipe(plumber())
-    .pipe(sourcemaps.init())
     .pipe(compiledSass({ outputStyle: 'expanded' }).on('error', compiledSass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist/css'))
+    .pipe(dest('dist/css', { sourcemaps: '.' }))
     .pipe(browserSync.stream());
 };
 
-// ---------- Watch ----------------
-export const watcher = () => {
-  watch('src/sass/**/*.scss', styles);
-  watch('src/*.html', html);
-};
-
-// ---------- HTML -----------------
+/* ---------- HTML ---------- */
 export const html = () => {
   return src('src/*.html')
     .pipe(htmlMin({ collapseWhitespace: true }))
     .pipe(dest('dist/'));
 };
 
-// ---------- Copy files ----------
+/* ---------- Copy files ---------- */
 export const copyFiles = () => {
   return src([
-      'src/fonts/**/*',
-      'src/*.ico',
-      'src/site.webmanifest',
-      'src/browserconfig.xml'
-    ], { base: 'src' })
+    'src/fonts/**/*',
+    'src/*.ico',
+    'src/site.webmanifest',
+    'src/browserconfig.xml',
+  ], { base: 'src' })
     .pipe(dest('dist/'));
 };
 
-// ---------- Copy images ----------
+/* ---------- Copy images ---------- */
 export const copyImages = () => {
-  return src('src/img/**/*.{png,jpg}')
+  return src('src/img/**/*.{png,jpg,jpeg,svg,gif,webp}')
     .pipe(dest('dist/img'));
 };
 
-// ---------- Optimize images ----------
+/* ---------- Optimize images ---------- */
 export const optimizeImages = () => {
-  return src('src/img/**/*.{png,jpg}')
-    .pipe(sharpOptimizeImages({
-      resize: false,
-      png: { quality: 80 },
-      jpeg: { quality: 80, mozjpeg: true },
-    }))
+  return src('src/img/**/*.{png,jpg,jpeg}')
+    .pipe(
+      sharpOptimizeImages({
+        resize: false,
+        png_to_png: { quality: 80 },
+        jpg_to_jpg: { quality: 80, mozjpeg: true },
+      })
+    )
     .pipe(dest('dist/img'));
 };
 
-// ---------- WebP ----------
+/* ---------- WebP ---------- */
 export const webpImages = () => {
-  return src('src/img/**/*.{jpg,png}')
+  return src('src/img/**/*.{jpg,jpeg,png}')
     .pipe(webp({ quality: 90 }))
     .pipe(dest('dist/img'));
 };
 
-// ---------- Main tasks -----------
+/* ---------- Watcher ---------- */
+export const watcher = () => {
+  watch('src/sass/**/*.scss', styles);
+  watch('src/*.html', series(html, browserSync.reload));
+  watch('src/img/**/*.{jpg,jpeg,png,svg,gif,webp}', series(copyImages, webpImages));
+};
+
+/* ---------- Main tasks ---------- */
 export const dev = series(
   clean,
   parallel(copyFiles, copyImages, styles, html, webpImages),
-  parallel(server, watcher),
+  parallel(server, watcher)
 );
 
 export const build = series(
@@ -104,5 +102,5 @@ export const build = series(
   parallel(copyFiles, optimizeImages, styles, html, webpImages)
 );
 
-// ---------- Default task ---------
+/* ---------- Default ---------- */
 export default dev;
